5a2aa97b8dd26464cd8a799c31606e6a2bce1e59
Juan Quino <quinojuan@gmail.com>
2022-12-21 09:49:16 -0300

    loading css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Loading from "./components/Loading";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(false);
+
+  const changeState = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  };
+
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div>
+        <br />
+        <br />
+        <button className="btn btn-primary" onClick={() => changeState()}>
+          Cargar página
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import { Spinner } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Loading() {
+  return (
+    <div>
+      <Spinner color="secondary" />
+    </div>
+  );
+}
+
+export default Loading;
